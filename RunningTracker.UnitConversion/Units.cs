169b761e2d9cf8cadfd93a03bb19b10169ed4965
Bryan Camp <bryancamp@Bryans-MacBook-Pro.local>
2019-01-16 16:58:10 -0600

    Add shell for unit conversion

---
+++ RunningTracker.UnitConversion/Units.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RunningTracker.UnitConversion
+{
+    public enum DistanceUnit
+    {
+        Centimeter,
+        Meter,
+        Kilometer,
+        Yard,
+        Inch,
+        Feet,
+        Meter,
+        Kilometer,
+        Mile
+    }
+
+    public enum TimeUnit
+    {
+        Nanoseconds,
+        Microsecond,
+        Millisecond,
+        Second,
+        Minute,
+        Hour,
+        Day,
+        Week,
+        Month,
+        Year        
+    }
+    
+    public class Speed
+    {
+        public Speed(TimeUnit timeUnit, DistanceUnit distanceUnit, decimal value)
+        {
+            Time = timeUnit;
+            Distance = distanceUnit;
+            Value = value;
+        }
+
+        public TimeUnit Time { get; }
+        public DistanceUnit Distance { get; }
+        public decimal Value { get; }
+    }
+}
